14d7ded706389e66ee79e39a4f081924d97ea084
vzhirov@mail.ru <vzhirov@mail.ru>
2020-12-25 14:51:37 +0300

    Created minimal "hello, wolrd" application with autotools build system

---
+++ config.h
@@ -0,0 +1,27 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "elvis_vzhirov_entry_test"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "vzhirov@mail.ru"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "elvis_vzhirov_entry_test"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "ELVIS_VZHIROV_ENTRY_TEST 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "elvis_vzhirov_entry_test"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
+
